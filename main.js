526d3badfbc39287fe70ab7e307c3ba0e795292c
Marko <82616233+milkoviicc@users.noreply.github.com>
2023-12-23 11:45:33 +0100

    Update main.js
    Fixed mobile menu bug on window resizing, it disappears if width of the window isn't <= 996px.

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-let mobileMenuBtn = document.querySelector('.mobile-menu-btn');
-let mobileMenu = document.querySelector('.mobile-menu');
-
-
 document.addEventListener('DOMContentLoaded', () => {
     let mobileMenuBtn = document.querySelector('.mobile-menu-btn');
     let mobileMenu = document.querySelector('.mobile-menu');
+    let width;
 
-    mobileMenuBtn.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden');
-        mobileMenu.classList.toggle('show');
+    function handleResize() {
+        width = window.innerWidth;
+        console.log(width);
+
+        if (width <= 996) {
+            if (mobileMenuBtn) {
+                mobileMenuBtn.addEventListener('click', () => {
+                    if (mobileMenu) {
+                        mobileMenu.classList.toggle('hidden');
+                        mobileMenu.classList.toggle('show');
+                    }
+                });
+            }
+        } else {
+            if (mobileMenu) {
+                mobileMenu.classList.remove('show');
+                mobileMenu.classList.add('hidden');
+            }
+        }
+    }
+
+    // Initial setup on DOMContentLoaded
+    handleResize();
+
+    // Update on window resize
+    window.addEventListener('resize', () => {
+        handleResize();
     });
 });
